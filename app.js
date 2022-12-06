f6fb504fefee2b8b0af998c967ba2ff4f0cfdb29
vaishnavi <vkawale2004@gmail.com>
2022-12-06 10:52:00 +0530

    update

---
+++ app.js
@@ -0,0 +1,32 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#inputText");
+var outputDiv = document.querySelector("#outputText");
+
+
+var serverURL = "https://api.funtranslations.com/translate/chef.json"
+
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("Something Wrong with Server! ")
+}
+
+
+function clickHandler() {
+    var inputText = txtInput.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
